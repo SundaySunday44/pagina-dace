fbcc697bc4ebc3f241bca442ab89b8a583f66bf6
dazawebtutoriales <dazawebtutoriales@gmail.com>
2024-11-13 23:48:28 -0500

    services-model 90%

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ContactoDaceComponent } from './contacto-dace/contacto-dace.component';
+import { ServiciosDaceComponent } from './servicios-dace/servicios-dace.component';
 
 export const routes: Routes = [
-    { path: 'contacto-dace', component: ContactoDaceComponent }
+    { path: 'contacto-dace', component: ContactoDaceComponent },
+    { path: 'servicios-dace', component : ServiciosDaceComponent }
 ];
