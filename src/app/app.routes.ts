c0d0e0b542aeb562e0c39467fc8d503b8475fb6a
Jdarioim <136277970+Jdarioim@users.noreply.github.com>
2024-11-17 21:55:44 -0500

    ComponentesUnidos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ContactoDaceComponent } from './contacto-dace/contacto-dace.component';
 import { ServiciosDaceComponent } from './servicios-dace/servicios-dace.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
 
 export const routes: Routes = [
     { path: '', component: InicioComponent },
     { path: 'contacto-dace', component: ContactoDaceComponent },
-    { path: 'servicios-dace', component : ServiciosDaceComponent }
+    { path: 'servicios-dace', component : ServiciosDaceComponent },
+    {path: 'quienes-somos', component: QuienesSomosComponent}
 ];
