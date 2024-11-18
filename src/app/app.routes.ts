44e23f58324b37629277dceacd76c8fd2711f40c
dazawebtutoriales <dazawebtutoriales@gmail.com>
2024-11-17 21:47:50 -0500

    lider + adrian funcional

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ContactoDaceComponent } from './contacto-dace/contacto-dace.component';
 import { ServiciosDaceComponent } from './servicios-dace/servicios-dace.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 export const routes: Routes = [
+    { path: '', component: InicioComponent },
     { path: 'contacto-dace', component: ContactoDaceComponent },
     { path: 'servicios-dace', component : ServiciosDaceComponent }
 ];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,32 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+    content: ["./src/**/*.{html,js}"],
 
-  theme: {
-    extend: {
-      animation: {
-        'fade-in': 'fadeIn 2s ease-in-out',
-        'slide-in': 'slideIn 1.5s ease-in-out',
-        'bounce': 'bounce 2s infinite',
-      },
-      keyframes: {
-        fadeIn: {
-          '0%': { opacity: 0 },
-          '100%': { opacity: 1 },
+    theme: {
+        extend: {
+            animation: {
+                'fade-in': 'fadeIn 2s ease-in-out',
+                'slide-in': 'slideIn 1.5s ease-in-out',
+                'bounce': 'bounce 2s infinite',
+            },
+            keyframes: {
+                fadeIn: {
+                    '0%': { opacity: 0 },
+                    '100%': { opacity: 1 },
+                },
+                slideIn: {
+                    '0%': { transform: 'translateX(-50%)', opacity: 0 },
+                    '100%': { transform: 'translateX(0)', opacity: 1 },
+                },
+            },
+            colors: {
+                'azul-container': '#207EA6',
+                'azul-container-2': '4EB5CF',
+                'gray-container': '3E3E3E',
+                'gray-7A7A7A': '#F2F2F2',
+                'gray-full': '#0000009E'
+            },
+
         },
-        slideIn: {
-          '0%': { transform: 'translateX(-50%)', opacity: 0 },
-          '100%': { transform: 'translateX(0)', opacity: 1 },
-        },
-      },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
