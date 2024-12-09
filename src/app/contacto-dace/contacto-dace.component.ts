5c5fcbc4097f1ad10140f49a48b63d72996ef40f
dazawebtutoriales <dazawebtutoriales@gmail.com>
2024-12-09 18:41:09 -0500

    sending messages wip

--- src/app/contacto-dace/contacto-dace.component.ts
+++ src/app/contacto-dace/contacto-dace.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto-dace',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contacto-dace.component.html',
   styleUrl: './contacto-dace.component.css'
 })
 export class ContactoDaceComponent {
+  
+  asunto: string = '';
+  mensaje: string = '';
+  numero: string = '593992766321';
 
+  enviarMensaje(){
+    if(this.mensaje.trim() && this.asunto.trim()){
+      const url = `https://wa.me/${this.numero}?text=${encodeURIComponent('Hola gracias por comunicarte, pronto te atenderemos: ASUNTO: ' + this.asunto + ' MENSAJE: ' + this.mensaje)}`;
+      window.open(url, '_blank');
+      this.asunto = '';
+      this.mensaje = '';
+    }else{
+      console.error('Por favor, completa todos los campos.');
+    }
+  }
 }
