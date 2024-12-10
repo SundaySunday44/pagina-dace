06dea4b426de0a86f5c61f30e9f8d26c5bdd4031
dazawebtutoriales <dazawebtutoriales@gmail.com>
2024-12-09 19:06:43 -0500

    Revert "sending messages wip"
    This reverts commit 5c5fcbc4097f1ad10140f49a48b63d72996ef40f.

--- src/app/contacto-dace/contacto-dace.component.ts
+++ src/app/contacto-dace/contacto-dace.component.ts
@@ -1,27 +1,12 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto-dace',
   standalone: true,
-  imports: [FormsModule],
+  imports: [],
   templateUrl: './contacto-dace.component.html',
   styleUrl: './contacto-dace.component.css'
 })
 export class ContactoDaceComponent {
-  
-  asunto: string = '';
-  mensaje: string = '';
-  numero: string = '593992766321';
 
-  enviarMensaje(){
-    if(this.mensaje.trim() && this.asunto.trim()){
-      const url = `https://wa.me/${this.numero}?text=${encodeURIComponent('Hola gracias por comunicarte, pronto te atenderemos: ASUNTO: ' + this.asunto + ' MENSAJE: ' + this.mensaje)}`;
-      window.open(url, '_blank');
-      this.asunto = '';
-      this.mensaje = '';
-    }else{
-      console.error('Por favor, completa todos los campos.');
-    }
-  }
 }
